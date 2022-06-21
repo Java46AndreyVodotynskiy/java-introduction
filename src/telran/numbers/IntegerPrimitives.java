36358ac1591cff16c1b05a9bb369b2a772deb9e9
macbook <macbook@mbp-macbook.lan>
2022-06-21 16:28:41 +0300

    adds HW #2 with method getLength for getting length of any type

--- src/telran/numbers/IntegerPrimitives.java
+++ src/telran/numbers/IntegerPrimitives.java
@@ -1,92 +1,100 @@
 package telran.numbers;
 
 public class IntegerPrimitives {
+
 	public static byte getMaxByte() {
 		byte res = 1;
-		while(res>0) {
-			res = (byte) ((int)res * 2);
+		while (res > 0) {
+			res = (byte) (res * 2);
+
 		}
-		return (byte) (res-1);
+		return (byte) (res - 1);
 	}
 
 	public static byte getMinByte() {
-		return (byte)(getMaxByte() + 1);
+		
+		return (byte) (getMaxByte() + 1);
 	}
 
-	public static byte getLengthByte() {
-		return (byte) getLength(Byte.SIZE);
+	public static int getLengthByte() {
+		return getLength(getMaxByte());
 	}
 
 	public static char getMaxChar() {
 		char res = 1;
-		while(res>0) {
-			res = (char) ((int)res * 2);
+		while (res > 0) {
+			res = (char) (res * 2);
+
 		}
-		return (char) (res-1);
+		return (char) (res - 1);
 	}
 
 	public static char getMinChar() {
-		return (char)(getMaxChar() + 1);
+
+		return (char) (getMaxChar() + 1);
 	}
 
 	public static int getLengthChar() {
-		System.out.println(Character.SIZE);
-		return getLength(Character.SIZE);
+		return getLength(getMaxChar());
 	}
 
 	public static short getMaxShort() {
 		short res = 1;
-		while(res>0) {
-			res= (short) ((int)res * 2);
+		while (res > 0) {
+			res = (short) (res * 2);
 		}
-		return (short) (res-1);
-	}
-	
-	public static short getMinShort() {
-		return (short)(getMaxShort() + 1);
+		return (short) (res - 1);
 	}
 
-	public static short getLengthShort() {
-		return (short) getLength(Short.SIZE);
+	public static short getMinShort() {
+		return (short) (getMaxShort() + 1);
 	}
-	
+
+	public static int getLengthShort() {
+		return getLength(getMaxShort());
+	}
+
 	public static int getMaxInt() {
 		int res = 1;
-		while(res > 0) {
-			res *= 2;
+		while (res > 0) {
+			res = (int) (res * 2);
+
 		}
-		return res-1;
+		return (int) (res - 1);
 	}
 
 	public static int getMinInt() {
-		return (getMaxInt() + 1);
+		return getMaxInt() + 1;
 	}
 
 	public static int getLengthInt() {
-		return getLength(Integer.SIZE);
+		return getLength(getMaxInt());
 	}
 
 	public static long getMaxLong() {
 		long res = 1;
-		while(res>0) {
-			res*=2;
+		while (res > 0) {
+			res = res * 2;
 		}
-		return res-1;
+		return res - 1;
 	}
 
 	public static long getMinLong() {
-		return (long)(getMaxLong() + 1);
+		return getMaxLong() + 1;
 	}
 
-	public static long getLengthLong() {
-		return getLength(Long.SIZE);
+	public static int getLengthLong() {
+		return getLength(getMaxLong());
 	}
 
 	private static int getLength(long x) {
-		int res = 8;
-		while(x != res) {
-			res = res * 2;
-		}
-		return res / 8;
+		long value = 1;
+		int count = 1;
+		while(value < x && value > 0) {
+			value *= 2;
+			count++;		}
+
+		return count / 8;
 	}
+
 }
