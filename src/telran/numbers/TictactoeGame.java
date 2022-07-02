f3423db553d60fa6bb8dcb6a5981e0ebcec71fea
macbook <macbook@mbp-macbook.lan>
2022-07-02 12:01:28 +0300

    adds CW #6 with TODO's for HW #6

--- src/telran/numbers/TictactoeGame.java
+++
@@ -1,8 +0,0 @@
-package telran.numbers;
-
-public class TictactoeGame {
-	public static int tictactoeMove(char matrix[][], int nRow, int nColumn, char symn) {
-		//TODO
-		return - 1;
-	}
-}
---
+++ src/telran/text/Strings.java
@@ -0,0 +1,16 @@
+package telran.text;
+
+import java.util.function.BooleanSupplier;
+
+public class Strings {
+	public static int[] deepNoRepeatedCompare(String str1,String str2) {
+		//TODO
+		return null;
+	}
+
+	public static BooleanSupplier isAnagram(String str1, String str3) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
